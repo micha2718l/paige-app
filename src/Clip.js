5b34e7feb3d2cc4b9d36d0a373fdf8c8cc1a9b3c
MIchael Haas <micha2718l@gmail.com>
2021-10-28 16:35:28 -0500

    donwload links

--- src/Clip.js
+++ src/Clip.js
@@ -6,6 +6,7 @@ function App({ clip_fn }) {
     <div style={{margin: "5%", backgroundColor: "black"}}>
       <div style={{margin: "1rem", padding: "0.5rem", backgroundColor: "white"}}>
           <h3>{ clip_fn }</h3>
+          <a href={"https://jason-paige.s3.amazonaws.com/" + clip_fn + ".mov"} download>DOWNLOAD</a>
           <Player
             playsInline
             preload="metadata"
